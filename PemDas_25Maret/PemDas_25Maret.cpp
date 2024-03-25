eb7a2b5c6188819420526c18fc94e9bc15f136b4
annisadiann <annisadian0412@gmail.com>
2024-03-25 10:45:02 +0700

    command menu

--- PemDas_25Maret/PemDas_25Maret.cpp
+++ PemDas_25Maret/PemDas_25Maret.cpp
@@ -33,16 +33,48 @@ int main()
 {
     int pilihan;
 
+    do {
+         system("cls");
+         cout << "======= Menu =======" << endl;
+         cout << "1. Penjumlahan" << endl;
+         cout << "2. Pengurangan" << endl;
+         cout << "3. Perkalian" << endl;
+         cout << "4. Pembagian" << endl;
+         cout << "5. Keluar" << endl;
+         cout << "Masukkan menu yang ingin di pilih (1/2/3/4/5): ";
+         cin >> pilihan;
 
-    system("cls");
-    cout << "======= Menu =======" << endl;
-    cout << "1. Penjumlahan" << endl;
-    cout << "2. Pengurangan" << endl;
-    cout << "3. Perkalian" << endl;
-    cout << "4. Pembagian" << endl;
-    cout << "5. Keluar" << endl;
-    cout << "Masukkan menu yang ingin di pilih (1/2/3/4/5): ";
-    cin >> pilihan;
+         switch (pilihan)
+         {
+         case 1:
+             input();
+             cout << "Hasil penjumlahan = " << penjumlahan(a, b) << endl;
+             system("pause");
+             break;
+         case 2:
+             input();
+             cout << "Hasil pengurangan = " << penjumlahan(a, b) << endl;
+             system("pause");
+             break;
+         case 3:
+             input();
+             cout << "Hasil perkalian = " << penjumlahan(a, b) << endl;
+             system("pause");
+             break;
+         case 4:
+             input();
+             cout << "Hasil pembagian = " << penjumlahan(a, b) << endl;
+             system("pause");
+             break;
+         case 5:
+             break;
+         default:
+             cout << "Menu tidak tersedia silahkan pilih lagi !!!" << endl;
+
+         }
+
+
+    } while (pilihan != 5);
 
 }
 
